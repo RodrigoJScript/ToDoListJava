e4138f4dbbdd3271d3266186b7dbfe35c9e9a7bb
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-09-06 12:31:30 -0600

    git init

---
+++ src/main/java/com/todolist/App.java
@@ -0,0 +1,29 @@
+package com.todolist;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("/com/todolist/primary.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
---
+++ src/main/java/com/todolist/PrimaryController.java
@@ -0,0 +1,9 @@
+package com.todolist;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+    @FXML
+    private TextField campoTexto;
+}
