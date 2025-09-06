a554e8895a2f38bf044c3d5c80f40a3e21b0e340
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-09-06 12:38:07 -0600

    feat: agregado sqlite al proyecto

--- src/main/java/com/todolist/PrimaryController.java
+++ src/main/java/com/todolist/PrimaryController.java
@@ -6,4 +6,11 @@ import javafx.scene.control.TextField;
 public class PrimaryController {
     @FXML
     private TextField campoTexto;
+
+    @FXML
+    private void agregarTarea() {
+        String tarea = campoTexto.getText();
+        System.out.println(tarea);
+        campoTexto.clear();
+    }
 }
